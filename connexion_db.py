1481f4d0df7cd15153e43a001eff693104064cfd
estebanbarthod <119411878+Estebanbrrr@users.noreply.github.com>
2024-02-25 22:27:28 +0100

    Add files via upload

---
+++ connexion_db.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+import pymysql.cursors
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv("./env.env")
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        #
+        db = g._database = pymysql.connect(
+            host=os.environ.get("HOST"),
+            user=os.environ.get("LOGIN"),
+            password=os.environ.get("PASSWORD"),
+            database=os.environ.get("DATABASE"),
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+
+        )
+    return db
